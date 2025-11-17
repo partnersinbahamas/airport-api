1844a0e315310dfd118a8e8fedce69e5a6ea7f36
Denys Bokov <denisbokov1703@gmail.com>
2025-11-17 20:24:24 +0100

    feat: add static and media urls

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -17,3 +18,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
+
+    # STATIC and MEDIA files
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
