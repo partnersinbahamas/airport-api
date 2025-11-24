e4289300132a515ab564bf65c80dba660013c2cf
Denys Bokov <denisbokov1703@gmail.com>
2025-11-24 20:37:12 +0100

    test(service): create new fixture

--- service/tests/fixtures.py
+++ service/tests/fixtures.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth import get_user_model
 
 from service.models import Airport
 
@@ -10,4 +11,36 @@ def create_airport_model(db):
         city="Hawkins",
         open_year=2025,
         image="image.png"
+    )
+
+@pytest.fixture()
+def create_airport_list(db):
+    raw_airports = [
+        Airport(
+            name="Airport Berlin",
+            city="Berlin",
+            open_year=2000,
+            image="image-berlin-airport.png"
+        ),
+        Airport(
+          name="Airport Paris",
+          city="Paris",
+          open_year=2001,
+          image="image-paris-airport.png"
+        ),
+        Airport(
+            name="Airport Kyiv",
+            city="Kyiv",
+            open_year=2002,
+            image="image-kyiv-airport.png"
+        )
+    ]
+
+    return Airport.objects.bulk_create(raw_airports)
+
+@pytest.fixture()
+def create_admin_user(db):
+    return get_user_model().objects.create_superuser(
+        username="admin",
+        password="adminpassword"
     )
