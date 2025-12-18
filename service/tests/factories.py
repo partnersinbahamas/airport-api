84afac148e436e2f8a938ed962ec447ff2721b89
Denys Bokov <denisbokov1703@gmail.com>
2025-12-18 18:34:06 +0100

    test(service): create Airplane fixture

--- service/tests/factories.py
+++ service/tests/factories.py
@@ -1,7 +1,8 @@
 import factory
 from django.contrib.auth import get_user_model
 
-from ..models import Airport, Route, AirplaneType, Manufacturer
+from ..constants import MAX_PILOT_CAPACITY
+from ..models import Airport, Route, AirplaneType, Manufacturer, Airplane
 from ..utils import generate_unique_letters_code
 
 
@@ -64,3 +65,22 @@ class ManufacturerFactory(factory.django.DjangoModelFactory):
     founded_year = factory.Faker("year")
     website = factory.Faker("url")
     logo = None
+
+
+class AirplaneFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Airplane
+
+    name = factory.Faker("name")
+    type = factory.SubFactory(AirplaneTypeFactory)
+    manufacturer = factory.SubFactory(ManufacturerFactory)
+    rows = factory.Faker("pyint", min_value=1, max_value=20)
+    seats_in_row = factory.Faker("pyint", min_value=1, max_value=10)
+    pilots_capacity = factory.Faker("pyint", min_value=1, max_value=MAX_PILOT_CAPACITY)
+    personal_capacity = factory.Faker("pyint", min_value=0, max_value=6)
+    year_of_manufacture = factory.Faker("year")
+    fuel_capacity_l = factory.Faker("pyint", min_value=1000, max_value=10000)
+    cargo_capacity_kg = factory.Faker("pyint", min_value=1000, max_value=10000)
+    max_speed_kmh = factory.Faker("pyint", min_value=100, max_value=1000)
+    max_distance_km = factory.Faker("pyint", min_value=1000, max_value=10000)
+    image = None
